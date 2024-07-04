909f5348f71e97bd3f8ee543205c3b647efcc370
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2024-07-04 17:47:32 +0300

    Added swagger documentation

--- app/Http/Controllers/api/PostController.php
+++ app/Http/Controllers/api/PostController.php
@@ -11,6 +11,10 @@ use App\Http\Requests\CreatePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Http\Responses\PostResponse;
 
+
+/**
+ * @OA\Info(title="API de Posts", version="1.0")
+ */
 class PostController extends Controller
 {
 
@@ -21,6 +25,22 @@ class PostController extends Controller
         $this->postResponse = $postResponse;
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/posts",
+     *     summary="Lista todos os posts ou filtra todos por tag",
+     *     @OA\Parameter(
+     *         name="tag",
+     *         in="query",
+     *         description="Nome da tag para filtrar posts",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Sucesso")
+     * )
+     */
     public function index(Request $request)
     {
         if ($request->has('tag')) {
@@ -28,11 +48,12 @@ class PostController extends Controller
             $tag = Tag::where('name', strtolower($tagName))->first();
             if (!$tag) return response()->json([], 200);
 
-            $posts = $tag->posts()->get()->load('tags');
+            $posts = $tag->posts()->get();
         } else {
-            $posts = Post::all()->load('tags');
+            $posts = Post::all();
         }
         
+        $posts->load('tags');
         $transformedPosts = $posts->map(function ($post) {
             $postAttributes = $post->getAttributes();
             $postAttributes['tags'] = $post->tags->pluck('name')->toArray();
@@ -42,6 +63,29 @@ class PostController extends Controller
         return $this->postResponse->success($transformedPosts);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/posts",
+     *     summary="Cria um novo post",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"title","author","content","tags"},
+     *             @OA\Property(property="title", type="string", example="Título do Post"),
+     *             @OA\Property(property="author", type="string", example="Autor do Post"),
+     *             @OA\Property(property="content", type="string", example="Conteúdo do Post"),
+     *             @OA\Property(
+     *                 property="tags",
+     *                 type="array",
+     *                 @OA\Items(type="string"),
+     *                 example={"tags", "do", "post"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="201", description="Post criado com sucesso"),
+     *     @OA\Response(response="422", description="Erro de validação")
+     * )
+     */
     public function store(CreatePostRequest  $request)
     {    
         $post = Post::create($request->all());
@@ -61,6 +105,20 @@ class PostController extends Controller
         return $this->postResponse->success($postAttributes, 201);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/posts/{id}",
+     *     summary="Exibe um post específico",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response="200", description="Sucesso"),
+     *     @OA\Response(response="404", description="Post não encontrado")
+     * )
+     */
     public function show(string $id)
     {
         $post = Post::find($id);
@@ -74,6 +132,36 @@ class PostController extends Controller
         return $this->postResponse->success($postAttributes);
     }
 
+    /**
+     * @OA\Put(
+     *     path="/api/posts/{id}",
+     *     summary="Atualiza um post específico",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"title","author","content","tags"},
+     *             @OA\Property(property="title", type="string", example="Título do Post Atualizado"),
+     *             @OA\Property(property="author", type="string", example="Autor do Post Atualizado"),
+     *             @OA\Property(property="content", type="string", example="Conteúdo do Post Atualizado"),
+     *             @OA\Property(
+     *                 property="tags",
+     *                 type="array",
+     *                 @OA\Items(type="string"),
+     *                 example={"tags", "do", "post", "atualizadas"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Post atualizado com sucesso"),
+     *     @OA\Response(response="404", description="Post não encontrado"),
+     *     @OA\Response(response="422", description="Erro de validação")
+     * )
+     */
     public function update(UpdatePostRequest $request, string $id)
     {
         $post = Post::find($id);
@@ -96,6 +184,20 @@ class PostController extends Controller
         return $this->postResponse->success($postAttributes);
     }
 
+     /**
+     * @OA\Delete(
+     *     path="/api/posts/{id}",
+     *     summary="Deleta um post específico",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response="204", description="Post deletado com sucesso"),
+     *     @OA\Response(response="404", description="Post não encontrado")
+     * )
+     */
     public function destroy(string $id)
     {
         $post = Post::find($id);
